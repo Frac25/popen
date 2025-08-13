3dea512f54edbb0a0e5bbd9d29a2e89b5b126799
sydubois <sydubois@42.com>
2025-08-13 10:26:50 +0200

    V0

---
+++ popen.c
@@ -0,0 +1,76 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+
+
+FILE* ft_popen(char* command, char* mode)
+{
+    FILE *fp;
+    int pip[2];
+    pid_t pid;
+
+    if(pipe(pip) == -1)
+    {
+        perror("pipe");
+        return NULL;
+    }
+
+    pid = fork();
+    if(pid < 0) // fork failed     
+    {
+        perror("fork");
+        close(pip[0]);
+        close(pip[1]);
+        return NULL;
+    }
+
+    if(pid == 0) // child
+    {
+        if(mode[0] == 'r')
+        {
+            close(pip[0]);
+            dup2(pip[1], STDOUT_FILENO);
+        }
+        if(mode[0] == 'w')
+        {
+            close(pip[1]);
+            dup2(pip[0], STDIN_FILENO);
+        }
+        execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+        exit(1);
+    }
+    if(mode[0] == 'r')
+    {
+        close(pip[1]);
+        fp = fdopen(pip[0],"r" );
+    }
+    if(mode[0] == 'w')
+    {
+        close(pip[0]);
+        fp = fdopen(pip[1],"w" );
+    }
+
+    return (fp);
+
+}
+
+
+int main(void)
+{
+    FILE *fp;
+    char buf[256];
+
+    fp = ft_popen("ls", "r");
+    if (fp == NULL) {
+        perror("popen");
+        return 1;
+    }
+
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        printf("%s", buf);
+    }
+
+    pclose(fp);
+    return 0;
+}
