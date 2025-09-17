6da7a4663eec348536413a9371c2378064137fe1
sydubois <sydubois@42.com>
2025-09-17 15:26:25 +0200

    test de commit

--- popen.c
+++ popen.c
@@ -6,79 +6,79 @@
 
 FILE* ft_popen(char* command, char* mode)
 {
-    FILE *fp;
-    int pip[2];
-    pid_t pid;
+	FILE *fp;
+	int pip[2];
+	pid_t pid;
 
 	if(mode[0] != 'r' && mode[0] != 'w')
 		return (NULL);
 
-    if(pipe(pip) == -1)
-    {
-        perror("pipe");
-        return NULL;
-    }
+	if(pipe(pip) == -1)
+	{
+		perror("pipe");
+		return NULL;
+	}
 
-    pid = fork();
-    if(pid < 0) // fork failed
-    {
-        perror("fork");
-        close(pip[0]);
-        close(pip[1]);
-        return NULL;
-    }
+	pid = fork();
+	if(pid < 0) // fork failed
+	{
+		perror("fork");
+		close(pip[0]);
+		close(pip[1]);
+		return NULL;
+	}
 
-    if(pid == 0) // child
-    {
-        if(mode[0] == 'r')
-        {
-            close(pip[0]);
+	if(pid == 0) // child
+	{
+		if(mode[0] == 'r')
+		{
+			close(pip[0]);
 			if(dup2(pip[1], STDOUT_FILENO) == -1)
 				exit(1);
 			close(pip[1]);
-        }
-        if(mode[0] == 'w')
-        {
-            close(pip[1]);
+		}
+		if(mode[0] == 'w')
+		{
+			close(pip[1]);
 			if(dup2(pip[0], STDIN_FILENO) == -1)
 				exit(1);
 			close(pip[0]);
-        }
-        execl("/bin/sh", "sh", "-c", command, (char *)NULL);
-        exit(1);
-    }
+		}
+		execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+		exit(1);
+	}
 
-    if(mode[0] == 'r')
-    {
-        close(pip[1]);
-        fp = fdopen(pip[0],"r" );
-    }
-    if(mode[0] == 'w')
-    {
-        close(pip[0]);
-        fp = fdopen(pip[1],"w" );
-    }
+	if(mode[0] == 'r')
+	{
+		close(pip[1]);
+		fp = fdopen(pip[0],"r" );
+	}
+	if(mode[0] == 'w')
+	{
+		close(pip[0]);
+		fp = fdopen(pip[1],"w" );
+	}
 
-    return (fp);
+	return (fp);
 
 }
 
 
 int main(void)
 {
-    FILE *fp;
-    char buf[256];
+	FILE *fp;
+	char buf[256];
 
-    fp = ft_popen("ls", "r");
-    if (fp == NULL) {
-        perror("popen");
-        return 1;
-    }
+	fp = ft_popen("ls", "r");
+	if (fp == NULL) {
+		perror("popen");
+		return 1;
+	}
 
-    while (fgets(buf, sizeof(buf), fp) != NULL) {
-        printf("%s", buf);
-    }
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		printf("%s", buf);
+	}
 
-    pclose(fp);
-    return 0;
+	pclose(fp);
+	return 0;
 }
